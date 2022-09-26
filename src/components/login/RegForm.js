8e6c881e0a44b22255e3930fcb2f0628c4268731
ZaharDomolego <domolegozahar@gmail.com>
2022-09-27 00:55:06 +0800

    fixed reg form component paddings

--- src/components/login/RegForm.js
+++ src/components/login/RegForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RegForm = () => {
   return (
-    <form id="Reg-form" className="d-flex flex-column m-auto">
+    <form id="Reg-form" className="d-flex flex-column m-auto p-2">
         <label>Создайте бесплатный аккаунт</label>
         <input type="text" autoComplete="off" placeholder="Логин" className="input-placeholder w-100"></input>
         <input type="password" autoComplete="off" placeholder="Пароль" className="input-placeholder w-100"></input>
