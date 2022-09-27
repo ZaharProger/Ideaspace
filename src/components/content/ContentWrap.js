c3e24a8af6e06b5c74f5df65b2c6d701e5cfeee8
ZaharDomolego <domolegozahar@gmail.com>
2022-09-27 18:42:46 +0800

    removed footer from content

--- src/components/content/ContentWrap.js
+++ src/components/content/ContentWrap.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 
 import NavBar from './navbar/NavBar';
 import Content from './content-body/Content';
-import Footer from './footer/Footer';
 
 const ContentWrap = () => {
     const [menuButtonStatus, changeMenuButtonStatus] = useState(false);
@@ -21,7 +20,6 @@ const ContentWrap = () => {
         <div id="Content-wrap" className="d-flex flex-column ms-auto me-auto">
             <NavBar menu_button_status={ menuButtonStatus } />
             <Content navbar_search_status={ navBarSearchStatus } />
-            <Footer />
         </div>
     )
 }
