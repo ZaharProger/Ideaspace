b91b0c36f4da43a96d98450ff0e73d026fa8980b
ZaharDomolego <domolegozahar@gmail.com>
2022-10-24 14:36:46 +0800

    added password length and similarity check

--- src/hooks/useFormValidation.js
+++ src/hooks/useFormValidation.js
@@ -4,6 +4,8 @@ const useFormValidation = () => {
 
         const emptyInputs = formInputs.filter(input => input.value == "");
         const invalidLoginInput = formInputs.filter(input => input.name == 'login' && input.value.includes(" "));
+        const invalidPasswordLength = formInputs.filter(input => input.name == 'password' && formId == 'Reg-form' && input.value.length < 8);
+        const formPasswords = formInputs.filter(input => input.name.includes('password') && formId == 'Reg-form');
 
         let errorMessage = "";
         let errorInputs = [];
@@ -14,6 +16,16 @@ const useFormValidation = () => {
         else if (invalidLoginInput.length != 0){
             errorMessage = "В логине нельзя использовать пробел!";
             errorInputs.push(invalidLoginInput[0]);
+        }
+        else if (invalidPasswordLength.length != 0){
+            errorMessage = "Длина пароля не менее 8 символов!";
+            errorInputs.push(invalidPasswordLength[0]);
+        }
+        else if (formPasswords.length != 0){
+            if (formPasswords[0].value != formPasswords[1].value){
+                errorMessage = "Пароли не совпадают!";
+                errorInputs.push(...formPasswords);
+            }
         }
 
         if (errorMessage == ""){
