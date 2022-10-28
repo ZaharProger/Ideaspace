232aaf405f84e31a6339424a9bc29cfa31b405e5
ZaharDomolego <domolegozahar@gmail.com>
2022-10-29 01:00:12 +0800

    added flexible navigation logic

--- src/components/content/navbar/NavBarListItem.js
+++ src/components/content/navbar/NavBarListItem.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const NavBarListItem = (props) => {
-    const navigate = useNavigate();
     //console.log('navbar-list-item');
     return (
-        <div className="Navbar-list-item d-flex flex-column" onClick={ () => navigate(props.navbar_item_props.route) }>
+        <div className="Navbar-list-item d-flex flex-column" onClick={ () => props.navbar_item_props.callback() }>
             <i className={ `fa-regular ${props.navbar_item_props.icon}` }></i>
             <span className="mt-2">{props.navbar_item_props.caption}</span>
         </div>
--- src/hooks/useButtonsPane.js
+++ src/hooks/useButtonsPane.js
@@ -1,17 +1,43 @@
-import { paneTemplates } from "../globalConstants";
+import { useNavigate } from "react-router-dom";
+
+import { localStorageKeys, paneTemplates, queryStringParams, routes } from "../globalConstants";
 import NavBarListItem from "../components/content/navbar/NavBarListItem";
 
 const useButtonsPane = (template) => {
+    const navigate = useNavigate();
     const buttonsPane = [];
 
     for (let i = 0; i < template.length; ++i){
         if (template == paneTemplates.navigation){
             const { key , icon, caption, route } = template[i];
+            let callback = null;
+            switch (route){
+                case routes.main:
+                case routes.settings:
+                case routes.create:
+                    callback = () => navigate(route);
+                    break;
+                case routes.auth:
+                    callback = async () => {
+                        const sessionId = localStorage.getItem(localStorageKeys.session_id);
+                        const response = await fetch(`/api/Users?${queryStringParams.session_id}=${sessionId}`, {
+                            method: 'DELETE'
+                        });
+
+                        if (response.ok){
+                            localStorage.removeItem(localStorageKeys.session_id);
+                            navigate(route);
+                        }
+                    }
+                    break;
+            }
+
             buttonsPane.push(
             <NavBarListItem key={ key } navbar_item_props={{
                 icon: icon,
                 caption: caption,
-                route: route
+                route: route,
+                callback: callback
             }}/>);
         }
         else if (template == paneTemplates.profile_footer){
