6b6b9f58903495729ca86e8a5cbb96bdc186c274
ZaharDomolego <domolegozahar@gmail.com>
2022-11-10 22:26:05 +0800

    some fixed in icon styles

--- src/components/content/content-body/post/PostFooter.js
+++ src/components/content/content-body/post/PostFooter.js
@@ -7,11 +7,9 @@ const PostFooter = () => {
             Array.from(footer.getElementsByTagName('i')).forEach(footerItem => {
                 footerItem.onmouseover = () => {
                     footerItem.classList.replace('fa-regular', 'fa-solid');
-                    footerItem.style.color = footerItem.classList.contains('fa-heart')? '#ee0000' : '#4848ca';
                 }
                 footerItem.onmouseleave = () => {
                     footerItem.classList.replace('fa-solid', 'fa-regular');
-                    footerItem.style.color = '#787878';
                 }
                 footerItem.onclick = () => {
                     if (!footerItem.classList.contains('post-footer-animation')){
